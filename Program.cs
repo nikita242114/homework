e48c613ab3f5ced88c5503d1d6561c8ecbece1aa
nikita242114 <5358711@mail.ru>
2023-04-10 18:29:45 +0500

    work

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Задача 2:
+
+Console.WriteLine("Ведите число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите второе чилсло");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+
+System.Console.WriteLine( + a + b);
+
+else System.Console.WriteLine($"Большее число: {a}, меньшее число: {b}");
